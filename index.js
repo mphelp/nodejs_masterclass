0b0f92ae28e5392aaebea50bb539ce01c435a497
Matthew Phelps <mphelps3@nd.edu>
2018-12-20 01:47:11 -0500

    CORRECTION: section is 3 not 2. Finished 'Parsing Request Paths'

--- index.js
+++ index.js
@@ -5,10 +5,22 @@
 
 // Dependencies
 var http = require('http');
+var url = require('url');
 
 // The server should respond to all requests w/ a string
 var server = http.createServer(function(req, res){
+
+	// Get url and parse
+	var parsedUrl = url.parse(req.url, true);
+
+	// Get path
+	var path = parsedUrl.pathname.replace(/^\/+|\/+$/g,'');
+
+	// Send response
 	res.end('Hello World\n');
+
+	// Log the request path
+	console.log('path: '+path);
 });
 
 // Start the server and have it listen on port 3000
