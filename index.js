a1050a70ee0bb9cf8b0b6e87ba65bf90030ca153
Matthew Phelps <mphelps3@nd.edu>
2018-12-21 00:45:13 -0500

    processing url query strings into obj

--- index.js
+++ index.js
@@ -11,10 +11,13 @@ var url = require('url');
 var server = http.createServer(function(req, res){
 
 	// Get url and parse
-	var parsedUrl = url.parse(req.url, true);
+	var parsedUrl = url.parse(req.url, true); // true ensures correct parsing of query below
 
 	// Get path
 	var trimmedpath = parsedUrl.pathname.replace(/^\/+|\/+$/g,'');
+
+	// Get the query string as an object
+	var queryObj = parsedUrl.query;
 
 	// Get the HTTP method
 	var method = req.method.toLowerCase();
@@ -24,7 +27,8 @@ var server = http.createServer(function(req, res){
 
 	// Log the request path
 	console.log('req received on path: '+trimmedpath
-		+' with method: '+method);
+		+' w/ method: '+method
+		+' w/ query params: '+queryObj);
 });
 
 // Start the server and have it listen on port 3000
