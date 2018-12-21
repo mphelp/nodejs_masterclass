45669cf81f7aa7b3ff763747822ca65eb5228b47
Matthew Phelps <mphelps3@nd.edu>
2018-12-21 00:57:57 -0500

    formatting

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ var server = http.createServer(function(req, res){
 	res.end('Hello World\n');
 
 	// Log the request path
-	console.log('req received on path: '+trimmedpath
-		+' w/ method: '+method
-		+' w/ query params: '+JSON.stringify(queryObjStr));
+	console.log('Request received on path ('+trimmedpath
+		+') w/ method ('+method
+		+') w/ query params: '+JSON.stringify(queryObjStr));
 });
 
 // Start the server and have it listen on port 3000
